5e5ff2e25be164b1e566fdde643083a53e8751e7
Mingyar Furtado <mingyar.furtado@gmail.com>
2022-08-05 23:59:35 -0300

    Rockelivery: Validating user cep

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,16 +1,21 @@
 defmodule Rockelivery.Users.Create do
-  alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.{Error, Repo, User, ViaCep.Client}
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
+  def call(%{"cep" => cep} = params) do
+    # case Client.get_cep_info(cep) |> IO.inspect() do
+    #   {:ok, _cep_info} -> create_user(params)
+    #   {:error, _result} = error -> error
+    # end
 
-  defp handle_insert({:ok, %User{}} = result), do: result
+    changeset = User.changeset(params)
 
-  defp handle_insert({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+      user
+    else
+      {:error, %Error{} = error} -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
+    end
   end
 end
